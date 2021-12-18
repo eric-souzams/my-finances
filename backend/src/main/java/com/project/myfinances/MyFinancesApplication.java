4dff047901f2f7746f231f67081700ef00b06d93
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2021-12-18 15:07:46 -0300

    fix(test/mvc): fix bug when run repository tests

--- backend/src/main/java/com/project/myfinances/MyFinancesApplication.java
+++ backend/src/main/java/com/project/myfinances/MyFinancesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
+//@EnableWebMvc
 public class MyFinancesApplication implements WebMvcConfigurer {
 
 	@Override
